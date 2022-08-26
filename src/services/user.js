17ddc2845b2897f6dda54066f9d673f81f42651d
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-08-26 08:42:19 -0300

    feat: Adiciona ao diretório de service o arquivo 'user' e a função que registra um usuário e retorna o token de autenticação.

---
+++ src/services/user.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../database/models');
+
+const SECRET = 'secretJWT';
+const TOKEN_CONFIG = {
+  algorithm: 'HS256',
+  expiresIn: '1d',
+};
+
+const createNewUser = async (req) => {
+  const { body: { displayName, email, password, image } } = req;
+  const { id } = User.create({ displayName, email, password, image });
+  const token = jwt.sign({ id }, SECRET, TOKEN_CONFIG);
+  req.userId = token;
+  return token;
+};
+
+module.exports = {
+  createNewUser,
+};
