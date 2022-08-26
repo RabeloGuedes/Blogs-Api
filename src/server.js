f6e1a524466c0cb6ef7a84c8e90c02d80eb57c8e
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-08-26 15:13:08 -0300

    feat: Adiciona rota categoryRoute ao server.

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const app = require('./api');
 const loginRoute = require('./routes/login');
 const userRoute = require('./routes/user');
+const categoryRoute = require('./routes/category');
 const error = require('./middlewares/error');
 
 // não remova a variável `API_PORT` ou o `listen`
@@ -16,5 +17,6 @@ app.listen(port, () => console.log('ouvindo porta', port));
 
 app.use('/login', loginRoute);
 app.use('/user', userRoute);
+app.use('/categories', categoryRoute);
 
 app.use(error.errorHandler);
