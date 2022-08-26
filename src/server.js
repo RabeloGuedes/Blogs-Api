d8fda275a3b15c9180e1d9c112b3282d639ec994
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-08-25 22:11:33 -0300

    feat: Adiciona o middleware de rota de login e o middleware de tratamento de erro ao index.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const app = require('./api');
+const loginRoute = require('./routes/login');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -10,3 +11,11 @@ app.get('/', (_request, response) => {
 });
 
 app.listen(port, () => console.log('ouvindo porta', port));
+
+app.use('/login', loginRoute);
+
+app.use((err, _req, res, next) => {
+  console.log(err);
+  res.status(500).json({ message: 'Erro no servidor!' });
+  next(err);
+});
