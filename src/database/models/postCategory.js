12ab27eba8d082241b777292c1228a782851a7cd
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-08-26 16:52:30 -0300

    feat: Cria o model de postCategory e  adiciona associação aos models de blogPost e Category.

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,42 @@
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'BlogPosts',
+        key: 'id',
+      },
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Categories',
+        key: 'id',
+      },
+    }
+  },
+  {
+    timestamps: false,
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'BlogPosts',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'Categories',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
+
+module.exports = PostCategory;
