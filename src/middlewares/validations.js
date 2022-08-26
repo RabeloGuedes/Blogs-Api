7f7056ce038bfd6826cb5f28207164a44753778e
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-08-26 09:00:50 -0300

    feat: Adiciona validação do displayName.

--- src/middlewares/validations.js
+++ src/middlewares/validations.js
@@ -17,7 +17,17 @@ const isLoginValid = rescue(async (req, res, next) => {
   next();
 });
 
+const isDisplayNameValid = rescue(async (req, res, next) => {
+  const { displayName } = req.body;
+  if (displayName.length < 8) {
+    return res.status(400).json({
+      message: '"displayName" length must be at least 8 characters long',
+    });
+  } next();
+});
+
 module.exports = {
   loginValidation,
   isLoginValid,
+  isDisplayNameValid,
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
-// const { loginValidation } = require('../middlewares/validations');
+const { isDisplayNameValid } = require('../middlewares/validations');
 
 const userController = require('../controllers/user');
 
 const userRoute = express.Router();
 
-userRoute.post('/', userController.createNewUser);
+userRoute.post('/', isDisplayNameValid, userController.createNewUser);
 
 module.exports = userRoute;
